a6df172e03aa8f4c34289072e155be6fba51110d
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2022-07-12 00:59:20 +0900

    [Bronze II] Title: 정수의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	int cnt = 1;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == ',')
+		{
+			cnt++;
+		}
+	}
+	cout << cnt << endl;
+
+}
