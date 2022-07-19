7025e40d6f94c44f43847b31f56a4242b87b1c8a
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2022-07-20 03:13:08 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,40 @@
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+
+
+unsigned long long  memo[10001];
+unsigned long long  Fibo(int n)
+{
+	if (n == 0)
+	{
+		return 0;
+	}
+	else if (n == 1)
+	{
+		return 1;
+	}
+
+	if (memo[n] != 0)
+	{
+		return memo[n];
+	}
+	else 
+	{
+		memo[n] = Fibo(n - 1) + Fibo(n - 2);
+		return memo[n];
+	}
+
+	
+}
+
+int main()
+{
+
+	int n;
+	cin >> n;
+
+	cout << Fibo(n) << endl;
+	
+	return 0;
+}
