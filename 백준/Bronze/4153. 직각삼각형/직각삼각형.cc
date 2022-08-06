f7bce2f93423b0bb1e59b70a5f5ddbd474c0b7d9
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2022-08-06 23:40:38 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	while (1)
+	{
+		cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0)
+		{
+			break;
+		}
+		if (a * a + b * b == c * c)
+		{
+			cout << "right" << endl;
+		}
+		else if (a * a + c * c == b * b)
+		{
+			cout << "right" << endl;
+		}
+		else if (b * b + c * c == a * a)
+		{
+			cout << "right" << endl;
+		}
+		else
+		{
+			cout << "wrong" << endl;
+		}
+		
+	}
+}
