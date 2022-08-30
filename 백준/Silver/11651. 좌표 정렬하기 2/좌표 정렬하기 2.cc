6065e7672c9344acbabf13f161277cc8d8b61df3
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2022-08-30 14:54:58 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 60 ms, Memory: 5972 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<algorithm>
+#include<array>
+using namespace std;
+
+bool compare(const int* a,const int* b)
+{
+
+	if (a[1] == b[1])
+	{
+		return a[0] < b[0];
+	}
+	return a[1] < b[1];
+}
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+	int** c = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		c[i] = new int[2];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cin >> c[i][0] >> c[i][1];
+	}
+	sort(c, c + n, compare);
+
+		
+	for (int i = 0; i < n; i++)
+	{
+		cout << c[i][0] << " " << c[i][1] << '\n';
+	}
+
+
+	for (int i = 0; i < n; i++)
+	{
+		delete[] c[i];
+	}
+	delete[] c;
+
+
+}
