ee3c4306db7573e194d2d06e47b34456d8d0465f
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2022-09-12 23:01:39 +0900

    [Silver V] Title: 정열적인 정렬, Time: 136 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int n;
+	cin >> n;
+	int* a = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	sort(a, a + n);
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << ' ';
+	}
+	
+}
