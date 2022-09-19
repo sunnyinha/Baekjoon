269d6e470c94fbd0daed63d44ad34fc75ca97dd7
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2022-09-19 23:39:50 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "Hello World, Judge " << i << "!" << '\n';
+	}
+	
+ 	
+}
