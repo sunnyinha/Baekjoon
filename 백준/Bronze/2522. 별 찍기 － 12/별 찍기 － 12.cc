e8f3bdfe467d24c5a63d7be7d1f4966a8531dbae
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2022-10-13 20:22:31 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+   
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n - i; j > 0; j--)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = n - i; j > 0; j--)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+   
+
+}
