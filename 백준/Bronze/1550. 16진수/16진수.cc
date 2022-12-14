88c578394ee1ae752dda014b1887a86e27da9c18
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2022-12-14 22:44:45 +0900

    [Bronze II] Title: 16진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int a;
+	cin >> hex >> a;
+	cout << dec <<a;
+
+
+}
