5214b5b43bb0e631d183b3f536e20dc5a8f2846f
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2023-01-17 21:26:24 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    for (int i = 1; i <= n; i++)
+    {
+        string s;
+        getline(cin, s);
+        
+        cout << i << ". " << s << '\n';
+    }
+    
+
+}
