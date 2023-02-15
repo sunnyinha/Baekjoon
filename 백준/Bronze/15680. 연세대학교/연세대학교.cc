e7dfac32daa263f12baf4ecb02d5cb622f3a87ac
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2023-02-15 23:31:35 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n == 0)
+	{
+		cout << "YONSEI";
+	}
+	else
+	{
+		cout << "Leading the Way to the Future";
+	}
+}
+
