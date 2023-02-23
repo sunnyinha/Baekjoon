98e0da8f0d2b3159a0e5f1fe65b3c4ad4229685e
sunnyinha <80040296+sunnyinha@users.noreply.github.com>
2023-02-23 23:29:28 +0900

    [Bronze IV] Title: Fill the Rowboats!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5300. Fill the Rowboats！/Fill the Rowboats！.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << i << " ";
+		if (i % 6 == 0 || i == n)
+		{
+			cout << "Go!" << " ";
+		}
+	}
+}
+
